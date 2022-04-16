78dc78234ae2732ced1555a0905d69cb6b85e034
partho2589 <parthomalo615@.com>
2022-04-17 01:14:31 +0600

    added login component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import About from './components/About/About';
 import Blog from './components/Blog/Blog';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Notfound from './components/Notfound/Notfound';
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
-     <Routes>
-       <Route path='/' element={<Home></Home>}></Route>
-       <Route path='/blog' element={<Blog></Blog>}></Route>
-       <Route path='/about' element={<About></About>}></Route>
-       <Route path='*' element={<Notfound></Notfound>}></Route>
-     </Routes>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<Notfound></Notfound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import  './Header.css'
 import {Link} from 'react-router-dom'
 const Header = () => {
     return (
-        <div>
+        <div className='container'>
             <nav>
                 <Link to='/home'>Home</Link>
+                <Link to ="/blog">Blog</Link>
+                <Link to ="/about">About</Link>
+                <Link to ="/login">Login</Link>
             </nav>
+            
         </div>
     );
 };
