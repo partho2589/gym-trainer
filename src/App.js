d1136c177d0cf9f40dc166d14e86f9487bb5a9ae
partho2589 <parthomalo615@.com>
2022-04-16 23:23:32 +0600

    add about, home, blog, notfound, header components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Notfound from './components/Notfound/Notfound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/blog' element={<Blog></Blog>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='*' element={<Notfound></Notfound>}></Route>
+     </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to='/home'>Home</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
